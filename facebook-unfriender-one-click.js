3f964b6277a94899db103881b6d52aa814f1a8ce
Joey <joeyfenny@gmail.com>
2019-05-11 18:07:13 +0800

    add facebook unfriender id data attribute
    so we can access element and capture event

--- facebook-unfriender-one-click.js
+++ facebook-unfriender-one-click.js
@@ -1,24 +1,24 @@
 function begin () {
-    console.log('"Facebook Unfriender - One Click" is running');
-    
-    function sendMessageToBackgroundScript (userId) {
-        // SEND MESSAGE TO BACKGROUND TAB (USER ID)
-        chrome.runtime.sendMessage({check: "test"});
-        chrome.runtime.sendMessage({check: userId});
-    }
+		console.log('"Facebook Unfriender - One Click" is running');
 
-    var friendElements = document.querySelectorAll("._5qo4");
+		function sendMessageToBackgroundScript (userId) {
+				chrome.runtime.sendMessage({check: "test"});
+				chrome.runtime.sendMessage({check: userId});
+		}
 
-    for (var i = 0; i < friendElements.length; i++) { // for each friend element
+		var friendElements = document.querySelectorAll("._5qo4");
 
-        var userId = friendElements[i].innerHTML.split("id=")[1].split("&")[0];
+		for (var i = 0; i < friendElements.length; i++) { // for each friend element
 
-        // if element doesn't already have a delete button, add one
-        if (friendElements[i].innerHTML.indexOf("facebook-unfriender-one-click") == -1) {
-            var deleteButtonLocation = elements[i].childNodes[1].childNodes[0].childNodes[1].childNodes[0];
-            deleteButtonLocation.innerHTML = deleteButtonLocation.innerHTML + "<div class='facebook-unfriender-one-click' onclick='" + sendMessageToBackgroundScript(userId) + "' style='color: red !important; float: right; margin-top: 1.5px; font-size: 17px; cursor: pointer;'><b>X</b></div>";
-        }
-    }
+				friendElements[i].setAttribute('data-facebook-unfriender-id', i);
+
+				var userId = friendElements[i].innerHTML.split("id=")[1].split("&")[0];
+
+				if (friendElements[i].innerHTML.indexOf("facebook-unfriender-one-click") == -1) { // if element doesn't already have a delete button, add one
+						var deleteButtonLocation = friendElements[i].childNodes[1].childNodes[0].childNodes[1].childNodes[0];
+						deleteButtonLocation.innerHTML = deleteButtonLocation.innerHTML + "<div class='facebook-unfriender-one-click' onclick='" + sendMessageToBackgroundScript(userId) + "' style='color: red !important; float: right; margin-top: 1.5px; font-size: 17px; cursor: pointer;'><b>X</b></div>";
+				}
+		}
 
 }
 begin();
