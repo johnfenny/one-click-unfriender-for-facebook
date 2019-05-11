b9cb0a83f6995091df13200fe2581e855d424a51
Joey <joeyfenny@gmail.com>
2019-05-11 21:14:13 +0800

    nctr passed through

--- facebook-unfriender-background.js
+++ facebook-unfriender-background.js
@@ -15,14 +15,11 @@ var __spin_r;
 var __spin_b;
 var __spin_t;
 var __user;
-
-var unref = hc_friends_tab;
-var __comet_req = false;
-
-// var uid = 100014477568045;
-// nctr[_mod]=pagelet_timeline_app_collection_603014086%3A2356318349%3A2
-// referrer
-
+var unref = "hc_friends_tab";
+var __comet_req = "false";
+var uid;
+var referrer;
+var nctr;
 
 // INTERCEPT FACEBOOK NETWORK REQUEST TO CAPTURE PARAMS REQUIRED TO CREATE REQUESTS
 chrome.webRequest.onBeforeRequest.addListener(function(details) {
@@ -47,7 +44,15 @@ chrome.webRequest.onBeforeRequest.addListener(function(details) {
 
 // LISTEN FOR MESSAGES SENT FROM CONTENT SCRIPT
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    console.log(message);
+    if(message.referrer) {
+        referrer = message.referrer;
+    }
+    if(message.uid) {
+        uid = message.uid;
+    }
+    if(message.nctr) {
+        nctr = message.nctr;
+    }
 });
 
 
@@ -69,32 +74,3 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
 //         "mode":"cors"
 //     }
 // );
-
-
-
-
-
-
-
-
-
-
-// function test () {
-    //     console.log("all", __user, __a, __dyn, __req, __be, __pc, dpr, __rev, __s, fb_dtsg, jazoest, __spin_r, __spin_b, __spin_t);
-    // };
-    // setTimeout(test, 10000);
-
-    // __user=603014086
-    // __a=1
-    // __dyn=7AgNe-4amaxx2u6aJGeFxqewKKEKAdy8Z9LFwxx-6ES2N6wAxu13y888eo9qyUcWAwDKaxeUW3K13z8S2S4o5eu58O5U7S4E9ohwoU8-1rG0HFU2BxCq7ooxu6U6O5oaUvwHwrEsxeEgy86CEymu4EhwIUa9u4-3CcGcwzwyU5W3u1yyUizHAy85iawiEKucxy6o98dU-rz8mgK7o884y8w9m6EdUco
-    // __req=v
-    // __be=1
-    // __pc=PHASED%3Aufi_home_page_pkg
-    // dpr=3
-    // __rev=1000691465
-    // __s=%3Az4up6b%3A9tfgj9
-    // fb_dtsg=AQHF0uz_nHi4%3AAQEm_mmFW2dV
-    // jazoest=22097
-    // __spin_r=1000691465
-    // __spin_b=trunk
-    // __spin_t=1557426537
--- facebook-unfriender-one-click.js
+++ facebook-unfriender-one-click.js
@@ -1,13 +1,11 @@
 function begin () {
-
 	console.log('Chrome Extension "Facebook Unfriender - One Click" is running on this page.');
-
 	chrome.runtime.sendMessage({referrer: document.location.href});
-
+	chrome.runtime.sendMessage({nctr: "nctr[_mod]="+document.querySelector("._3sz").parentElement.outerHTML.split('aria-controls="')[1].split('"')[0]});
+	
 	var friendElements = document.querySelectorAll("._5qo4");
 
 	for (var i = 0; i < friendElements.length; i++) { // for each friend element
-
 		var userId = friendElements[i].innerHTML.split("user.php?id=")[1].split("&")[0];
 
 		if (friendElements[i].innerHTML.indexOf("facebook-unfriender-one-click") == -1) { // if element doesn't already have a delete button, add one
@@ -18,9 +16,9 @@ function begin () {
 
 	document.addEventListener('click', function(e) {
 		if (e.target.dataset.facebookUnfrienderId) {
-			chrome.runtime.sendMessage({userId: e.target.dataset.facebookUnfrienderId});
+			chrome.runtime.sendMessage({uid: e.target.dataset.facebookUnfrienderId});
 		}
 	});
-
+	// add setInterval code below this line
 }
 begin();
