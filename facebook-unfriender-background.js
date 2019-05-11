613418f2ec2595c6a19da1e5c5be102428b3205c
Joey <joeyfenny@gmail.com>
2019-05-11 17:38:17 +0800

    cleaner

--- facebook-unfriender-background.js
+++ facebook-unfriender-background.js
@@ -25,6 +25,7 @@ chrome.webRequest.onBeforeRequest.addListener(function(details) {
     console.log(details.requestBody);
 },{urls: ["<all_urls>"]},['requestBody']);
 
+// CHECK FOR MESSAGE SENT FROM FOREGROUND TAB
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
     if(msg.check) {
         console.log(msg.check);
