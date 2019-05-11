3f0889bdf940a70f5f5413b40cc8b191c6ed597d
Joey <joeyfenny@gmail.com>
2019-05-11 18:51:58 +0800

    pass user id to background script

--- facebook-unfriender-one-click.js
+++ facebook-unfriender-one-click.js
@@ -1,24 +1,23 @@
 function begin () {
 		console.log('"Facebook Unfriender - One Click" is running');
 
-		function sendMessageToBackgroundScript (userId) {
-				chrome.runtime.sendMessage({check: "test"});
-				chrome.runtime.sendMessage({check: userId});
-		}
-
 		var friendElements = document.querySelectorAll("._5qo4");
 
 		for (var i = 0; i < friendElements.length; i++) { // for each friend element
 
-				friendElements[i].setAttribute('data-facebook-unfriender-id', i);
-
-				var userId = friendElements[i].innerHTML.split("id=")[1].split("&")[0];
+				var userId = friendElements[i].innerHTML.split("user.php?id=")[1].split("&")[0];
 
 				if (friendElements[i].innerHTML.indexOf("facebook-unfriender-one-click") == -1) { // if element doesn't already have a delete button, add one
 						var deleteButtonLocation = friendElements[i].childNodes[1].childNodes[0].childNodes[1].childNodes[0];
-						deleteButtonLocation.innerHTML = deleteButtonLocation.innerHTML + "<div class='facebook-unfriender-one-click' onclick='" + sendMessageToBackgroundScript(userId) + "' style='color: red !important; float: right; margin-top: 1.5px; font-size: 17px; cursor: pointer;'><b>X</b></div>";
+						deleteButtonLocation.innerHTML = deleteButtonLocation.innerHTML + "<div class='facebook-unfriender-one-click' data-facebook-unfriender-id="+userId+" style='font-weight: bold; color: red !important; float: right; margin-top: 1.5px; font-size: 17px; cursor: pointer;'>X</div>";
 				}
 		}
+
+		document.addEventListener('click', function(e) {
+			if (e.target.dataset.facebookUnfrienderId) {
+				chrome.runtime.sendMessage({check: e.target.dataset.facebookUnfrienderId});
+			}
+		});
 
 }
 begin();
